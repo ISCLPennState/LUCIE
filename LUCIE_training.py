2a7c706d060eed27736feab25659ef911a0a8762
Ivanhg <hzg18@psu.edu>
2024-09-22 21:40:14 -0400

    Add files via upload

--- LUCIE_training.py
+++ LUCIE_training.py
@@ -103,51 +103,78 @@ test_u = target_dataset[ntrain:ntrain+ntest,:,:,:]
 
 
 train_set = TensorDataset(train_a, train_u)
-train_loader = DataLoader(train_set, batch_size=1, shuffle=False)
+train_loader = DataLoader(train_set, batch_size=16, shuffle=True)
 
 test_set = TensorDataset(test_a, test_u)
-test_loader = DataLoader(test_set, batch_size=1, shuffle=False)
+test_loader = DataLoader(test_set, batch_size=16, shuffle=False)
 
 
 
-def train_model_fine(model, train_set, test_set, optimizer, scheduler=None, nepochs=20, nfuture=0, num_examples=256, num_valid=8, loss_fn='l2', reg_rate=0):
+import time
+
+def spectral_regularizer(prd, tar, relative=False, squared=True):
+    # compute coefficients
+    diff = (prd-tar)
+
+    shtdiff = sht(diff)
+    coeffs = torch.view_as_real(shtdiff)
+
+    coeffs = coeffs[..., 0]**2 + coeffs[..., 1]**2    # take the real part only
+
+    norm2 = 2 * torch.sum(coeffs[..., :, 24:], dim=-1)  # regularize the wave number beyond 5
+    loss_reg = torch.sum(norm2, dim=(-1,-2))
+
+    if not squared:
+        loss_reg = torch.sqrt(loss_reg)
+    loss_reg = loss_reg.mean()
+
+    return loss_reg
+
+num_elements = 48
+
+mse_loss = nn.MSELoss()
+def train_model(model, train_loader, test_loader, optimizer, scheduler=None, nepochs=20, nfuture=0, num_examples=256, num_valid=8, loss_fn='l2', reg_rate=0):
 
     train_start = time.time()
 
-    train_loader_1 = DataLoader(train_set, batch_size=16, shuffle=True)
-    test_loader_1 = DataLoader(test_set, batch_size=16, shuffle=True)
 
-    infer_bias = 1e+20
-    recall_count = 0
-    for epoch in tqdm(range(nepochs)):
-        if epoch < 149:
-            if scheduler is not None:
-                scheduler.step()
-        else:
-            for param_group in optimizer.param_groups:
-                param_group['lr'] = 1e-6
+    for epoch in range(nepochs):
         epoch_start = time.time()
 
         optimizer.zero_grad()
         acc_loss = 0
 
         model.train()
-        batch_num = 0
+        i = 0
         for inp, tar in train_loader_1:
-            batch_num += 1
+            i += 1
             loss = 0
             inp = inp.to(device)
             tar = tar.to(device)
             prd = model(inp)
 
-            if loss_fn == 'l2':
-                loss_delta = l2loss_sphere(prd[:,:5,:,:], tar[:,:5,:,:], relative=True)
-                #loss_tp = SpectralLoss(img_size=(48, 96), absolute=True)(prd[:,5:,:,:].to("cpu"),tar[:,5:,:,:].to("cpu"))
-                loss_tp = torch.mean((prd[:,5:,:,:]-tar[:,5:,:,:])**2)
-                loss = (loss_delta.to(device)*5 + loss_tp.to(device))/6
-            elif loss_fn == "spectral l2":
-                loss = SpectralLoss(img_size=(48, 96), absolute=True)(prd.to("cpu"),tar.to("cpu"))
+            loss_delta = l2loss_sphere(prd[:,:5,:,:], tar[:,:5,:,:], relative=True)
+            loss_tp = torch.mean((prd[:,5:,:,:]-tar[:,5:,:,:])**2)
+            loss = (loss_delta.to(device)*5 + loss_tp.to(device))/6
 
+            
+            lat_index = np.r_[7:15, 32:40]
+            # lat_index = np.r_[0:48]
+            quad_weight_reg = quad_weights.reshape(1,1,48,1)[:,:,lat_index,:]
+            out_fft = torch.mean(torch.abs(torch.fft.rfft(prd[:,:,lat_index,:],dim=3)),dim=2)
+            target_fft = torch.mean(torch.abs(torch.fft.rfft(tar[:,:,lat_index,:],dim=3)),dim=2)
+            
+
+            # wave_weight = torch.arange(48).reshape(1,1,48).to(device)
+            wave_index = np.r_[0:48]
+            #wave_index = np.r_[7:15,32:40]
+            loss_fft = torch.abs(out_fft[:,:,wave_index]-target_fft[:,:,wave_index])# * cos_weight_reg
+
+            loss_reg = torch.mean(loss_fft[...,:]) 
+
+
+
+            loss = (1-reg_rate) * loss + reg_rate * loss_reg
 
             prd = prd.to(device)
             roll_loss = 0
@@ -160,86 +187,53 @@ def train_model_fine(model, train_set, test_set, optimizer, scheduler=None, nepo
 
         if scheduler is not None:
             scheduler.step()
+        model.eval()
 
-        if (epoch+1) % 10 == 0:
-            with torch.no_grad():
-                pred_frame = input_dataset[0].reshape(1,7,48,96) # T, SH, U, V, SP, TISR, ORO
-                pred_frame = pred_frame.to(device)
-                temp_bias = torch.zeros(48,96).to(device)
-                for k in range(2920):
-                    previous_frame = pred_frame[:,:5,:,:]
-                    pred_frame = model(pred_frame) # 6
-                    pred_frame[:,:5,:,:] = pred_frame[:,:5,:,:] * target_stds[:,:5,:,:] # T, SH, U, V, SP, TP
-                    # pred_frame = (pred_frame + 1) / 2 * (target_maxs - target_mins) + target_mins
+        if reg_rate != 0:
+            if (epoch+1) % 20 == 0:
+                with torch.no_grad():
+                    pred_frame = input_dataset[0].reshape(1,7,48,96) # T, SH, U, V, SP, TISR, ORO
+                    pred_frame = pred_frame.to(device)
+                    temp_bias = torch.zeros(48,96).to(device)
+                    for k in range(2920):
+                        previous_frame = pred_frame[:,:5,:,:]
+                        pred_frame = model(pred_frame) # 6
+                        pred_frame[:,:5,:,:] = pred_frame[:,:5,:,:] * target_stds[:,:5,:,:] # T, SH, U, V, SP, TP
+                        # pred_frame = (pred_frame + 1) / 2 * (target_maxs - target_mins) + target_mins
 
-                    pred_frame[:,:5,:,:] += previous_frame[:,:5,:,:] * input_stds + input_means
-                    tp_frame = pred_frame[:,5:,:,:] * tp_std + tp_mean
-                    # pred_frame += (previous_frame + 1) / 2 * (input_maxs - input_mins) + input_mins
-                    plot_frame = torch.cat((pred_frame[:,:5,:,:], tp_frame), 1) # T, SH, U, V, SP, TP
+                        pred_frame[:,:5,:,:] += previous_frame[:,:5,:,:] * input_stds + input_means
+                        tp_frame = pred_frame[:,5:,:,:] * tp_std + tp_mean
+                        # pred_frame += (previous_frame + 1) / 2 * (input_maxs - input_mins) + input_mins
+                        plot_frame = torch.cat((pred_frame[:,:5,:,:], tp_frame), 1) # T, SH, U, V, SP, TP
 
-                    pred_frame = pred_frame[:,:5,:,:]
-                    pred_frame = (pred_frame - input_means) / input_stds
-                    if k >= 1460:
-                        temp_bias += pred_frame[0,0,:,:].clone().detach()
-                    # pred_frame = 2 * (pred_frame - input_mins) / (input_maxs - input_mins) - 1
+                        pred_frame = pred_frame[:,:5,:,:]
+                        pred_frame = (pred_frame - input_means) / input_stds
+                        if k >= 1460:
+                            temp_bias += pred_frame[0,0,:,:].clone().detach()
+                        # pred_frame = 2 * (pred_frame - input_mins) / (input_maxs - input_mins) - 1
 
-                    pred_frame = torch.cat((pred_frame, input_dataset[k+1,5:,:,:].reshape(1,2,48,96).to(device)), dim=1)
-                    
-                temp_bias = torch.mean(torch.abs(temp_bias / 1460 - true_temp_clim))
-                print(temp_bias)
-                validation_bias.append(temp_bias)
-                if epoch > 1:
-                    if temp_bias <= infer_bias:
-                        infer_bias = temp_bias
-                        torch.save(model.state_dict(), 'LUCIE_method_results/regular_training_checkpoint.pth')
-                        recall_count = 0
-                    else:
-                        print('recall')
-                        state_pth = torch.load('LUCIE_method_results/regular_training_checkpoint.pth')
-                        model.load_state_dict(state_pth)
-                        recall_count += 1
-                        if recall_count > 3:
-                            print(epoch)
-                            break
+                        pred_frame = torch.cat((pred_frame, input_dataset[k+1,5:,:,:].reshape(1,2,48,96).to(device)), dim=1)
                         
-                    
-                plot_frame = plot_frame.clone().detach().cpu().numpy()
-                fig = plt.figure(figsize=(14, 4))
-                plt.subplot(1,7,1)
-                plt.imshow(plot_frame[0][0], origin="lower")
-                plt.axis("off")
-                plt.colorbar(shrink=0.3)
-                plt.subplot(1,7,2)
-                plt.imshow(plot_frame[0][1], origin="lower")
-                plt.axis("off")
-                plt.colorbar(shrink=0.3)
-                plt.subplot(1,7,3)
-                plt.imshow(plot_frame[0][2], origin="lower")
-                plt.axis("off")
-                plt.colorbar(shrink=0.3)
-                plt.subplot(1,7,4)
-                plt.imshow(plot_frame[0][3], origin="lower")
-                plt.axis("off")
-                plt.colorbar(shrink=0.3)
-                plt.subplot(1,7,5)
-                plt.imshow(plot_frame[0][4], origin="lower")
-                plt.axis("off")
-                plt.colorbar(shrink=0.3)
-                plt.subplot(1,7,6)
-                plt.imshow(plot_frame[0][5], origin="lower")
-                plt.axis("off")
-                plt.colorbar(shrink=0.3)
-                plt.show()
-
+                    temp_bias = torch.mean(torch.abs(temp_bias / 1460 - true_temp_clim))
+                    if epoch > 60:
+                        if temp_bias <= infer_bias:
+                            infer_bias = temp_bias
+                            torch.save(model.state_dict(), 'LUCIE_method_results/regular_training_checkpoint_0.pth')
+                            recall_count = 0
+                        else:
+                            print('recall')
+                            state_pth = torch.load('LUCIE_method_results/regular_training_checkpoint_0.pth')
+                            model.load_state_dict(state_pth)
+                            recall_count += 1
+                            if recall_count > 3:
+                                print(epoch)
+                                break
 
 
 
 torch.manual_seed(1447)
 torch.cuda.manual_seed(1447)
 torch.cuda.empty_cache()
-
-std = 0.05
-mean = 0
 
 grid='legendre-gauss'
 nlat = 48
@@ -264,5 +258,7 @@ model = SphericalFourierNeuralOperatorNet(params = {}, spectral_transform='sht',
 from torch.optim.lr_scheduler import OneCycleLR, CosineAnnealingLR, StepLR
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=0)
 scheduler = CosineAnnealingLR(optimizer, T_max=150, eta_min=1e-5)
-train_model(model, train_set, test_set, optimizer, scheduler=scheduler, nepochs=150, loss_fn = "l2")
+train_model(model, train_set, test_set, optimizer, scheduler=scheduler, nepochs=300, loss_fn = "l2")
+# fft_regularizer
+train_model(model, train_set, test_set, optimizer, scheduler=scheduler, nepochs=15, loss_fn = "l2", reg_rate=0.05)
 # torch.save(model.state_dict(), 'LUCIE.pth')
